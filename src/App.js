609ae617b895a061e0c1ed46bd0b67b440bb38f2
Jose Bautista <Josefbautista94@gmail.com>
2023-02-22 18:44:16 -0500

    Fixed up my App.js by setting up the Router , Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage.component";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
